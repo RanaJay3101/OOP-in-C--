660e1d56831bf639274af044e495bf2ee5feabcc
Rana Jayyyy <20bce241@nirmauni.ac.in>
2024-01-08 14:21:12 +0530

    oop in c++

---
+++ 20_oop.cpp
@@ -0,0 +1,11 @@
+/***
+ * object oriented programming
+ * 
+ * classes solves the problem of data and function by modelling program as a real world scenario
+ * 
+ * class is a blueprint of an object
+ * 
+ * object is an instance of a class
+ * 
+ * 
+*/
